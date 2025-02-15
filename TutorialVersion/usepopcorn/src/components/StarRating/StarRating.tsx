bd8b2bf65c2d99a38d78cb876cdaed54896503da
alexprimak58 <aprimak43@gmail.com>
2025-02-15 23:56:15 +0200

    Added Reusable Star Component

--- TutorialVersion/usepopcorn/src/components/StarRating/StarRating.tsx
+++ TutorialVersion/usepopcorn/src/components/StarRating/StarRating.tsx
@@ -3,26 +3,59 @@ import Star from "./Star";
 
 interface StartRatingProps {
   maxRating?: number;
+  color?: string;
+  size?: number;
+  className?: string;
+  messages?: string[];
+  defaultRating?: number;
+  onSetRating?: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function StartRating({ maxRating = 5 }: StartRatingProps) {
-  const [rating, setRating] = useState(0);
+export default function StartRating({
+  maxRating = 5,
+  color = "#FCC419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}: StartRatingProps) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating: number) {
+    setRating(rating);
+    // So if we want to have state of that rating somewhere outside that component, we can set onSetRating and paste function that will set the state in your application
+    onSetRating?.(rating);
+  }
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {/* Create an array with 5 elements and iterate over it */}
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            onRate={() => handleRating(i + 1)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(rating)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
@@ -35,9 +68,4 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
-};
-
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
 };
--- TutorialVersion/usepopcorn/src/index.tsx
+++ TutorialVersion/usepopcorn/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
@@ -7,9 +7,25 @@ import StarRating from "./components/StarRating/StarRating";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div className=''>
+      <StarRating color='blue' onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  );
+}
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating />
+    <StarRating
+      color={"red"}
+      messages={["terrible", "bad", "okay", "good", "awesome"]}
+      defaultRating={4}
+    />
+    <Test />
   </React.StrictMode>
 );
